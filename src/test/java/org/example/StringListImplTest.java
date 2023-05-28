f1d3f5d67a2ec41a5dfc8240de7ad1681cfcc11d
Denis_vrn <denisfedorov50@gmail.com>
2023-05-28 23:59:28 +0300

    it's not finish.....

--- src/test/java/org/example/StringListImplTest.java
+++ src/test/java/org/example/StringListImplTest.java
@@ -12,48 +12,18 @@ class StringListImplTest {
         String actual = stringList.add(STR1);
         Assertions.assertEquals(STR1,actual);
     }
-    @Test
-    void shouldReturnExceptionWhenAddNull() {
-        Assertions.assertThrows(NullPointerException.class, () -> stringList.add(STR_NULL));
-    }
 
     @Test
     void shouldReturnItemWhenAddItemByIndex() {
-        String actual = stringList.add(IND_POSITIVE, STR1);
+        String actual = stringList.add(0, STR1);
         Assertions.assertEquals(STR1,actual);
-    }
-
-    @Test
-    void shouldReturnExceptionWhenAddNullItemByIndex() {
-        Assertions.assertThrows(NullPointerException.class, () -> stringList.add(IND_POSITIVE, STR_NULL));
-    }
-    @Test
-    void shouldReturnExceptionWhenAddItemByNegativeIndex() {
-        Assertions.assertThrows(NullPointerException.class, () -> stringList.add(IND_NEGATIVE, STR1));
-    }
-    @Test
-    void shouldReturnExceptionWhenAddItemByIndexOverSize() {
-        Assertions.assertThrows(NullPointerException.class, () -> stringList.add(OVER_SIZE, STR1));
     }
 
     @Test
     void shouldReturnItemWhenSetItemByIndex() {
-        String actual = stringList.set(IND_POSITIVE, STR1);
+        String actual = stringList.set(0, STR1);
         Assertions.assertEquals(STR1,actual);
     }
-    @Test
-    void shouldReturnExceptionWhenSetNullItemByIndex() {
-        Assertions.assertThrows(NullPointerException.class, () -> stringList.set(IND_POSITIVE, STR_NULL));
-    }
-    @Test
-    void shouldReturnExceptionWhenSetItemByNegativeIndex() {
-        Assertions.assertThrows(NullPointerException.class, () -> stringList.set(IND_NEGATIVE, STR1));
-    }
-    @Test
-    void shouldReturnExceptionWhenSetItemByIndexOverSize() {
-        Assertions.assertThrows(NullPointerException.class, () -> stringList.set(OVER_SIZE, STR1));
-    }
-
     @Test
     void shouldReturnItemWhenRemoveByItem() {
         stringList.add(STR1);
@@ -65,14 +35,6 @@ class StringListImplTest {
         stringList.add(STR1);
         String actual = stringList.remove(0);
         Assertions.assertEquals(STR1,actual);
-    }
-    @Test
-    void shouldReturnExceptionWhenRemoveByNullItem() {
-        Assertions.assertThrows(NullPointerException.class, () -> stringList.remove(STR_NULL));
-    }
-    @Test
-    void shouldReturnExceptionWhenRemoveByNegativeIndex() {
-        Assertions.assertThrows(NullPointerException.class, () -> stringList.remove(IND_NEGATIVE));
     }
 
     @Test
@@ -87,40 +49,25 @@ class StringListImplTest {
         boolean actual = stringList.contains(STR2);
         Assertions.assertEquals(FALSE,actual);
     }
-    @Test
-    void shouldReturnExceptionContainsWhenNullItem() {
-        Assertions.assertThrows(NullPointerException.class, () -> stringList.contains(STR_NULL));
+
+//    @Test
+//    void indexOf() {
+//    }
+//
+//    @Test
+//    void lastIndexOf() {
+//    }
+//
+//    @Test
+//    void get() {
+//    }
+//
+//    @Test
+//    void testEquals() {
+//    }
+//
+//    @Test
+//    void size() {
     }
 
-    @Test
-    void indexOf() {
-    }
 
-    @Test
-    void lastIndexOf() {
-    }
-
-    @Test
-    void get() {
-    }
-
-    @Test
-    void testEquals() {
-    }
-
-    @Test
-    void size() {
-    }
-
-    @Test
-    void isEmpty() {
-    }
-
-    @Test
-    void clear() {
-    }
-
-    @Test
-    void toArray() {
-    }
-}
